8a0e9b42d5f12700fad32bf47b9f45571b3f1d60
BuildTools <unconfigured@null.spigotmc.org>
2024-10-07 14:01:53 +0200

    Initial commit

---
+++ src/main/java/net/isaac/isaacsmod/IsaacsModClient.java
@@ -0,0 +1,10 @@
+package net.isaac.isaacsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IsaacsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
