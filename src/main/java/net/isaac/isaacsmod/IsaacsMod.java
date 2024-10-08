73467e8db56109b129b3a50ffa5e2cf0bd03bcbd
BuildTools <unconfigured@null.spigotmc.org>
2024-10-08 17:12:17 +0200

    Added Blocks and Recipes Tweaked MODID to avoid conflicts

--- src/main/java/net/isaac/isaacsmod/IsaacsMod.java
+++ src/main/java/net/isaac/isaacsmod/IsaacsMod.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 
 public class IsaacsMod implements ModInitializer {
 	public static final String MOD_ID = "isaacsmod";
-	public static final int AGE = 18;
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -22,6 +21,6 @@ public class IsaacsMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("I thank thee for using my mod");
 	}
 }
