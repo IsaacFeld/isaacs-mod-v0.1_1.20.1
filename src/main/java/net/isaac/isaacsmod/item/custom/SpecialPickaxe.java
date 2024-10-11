3249daf19e5fe07399454d54fec2d8999c18f338
BuildTools <unconfigured@null.spigotmc.org>
2024-10-11 13:56:00 +0200

    Finished Tully's Pickaxe, Durability, Explosion, and Mining Levels are completely fixed

--- src/main/java/net/isaac/isaacsmod/item/custom/SpecialPickaxe.java
+++ src/main/java/net/isaac/isaacsmod/item/custom/SpecialPickaxe.java
@@ -1,35 +1,52 @@
 package net.isaac.isaacsmod.item.custom;
 
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.isaac.isaacsmod.IsaacsMod;
 import net.isaac.isaacsmod.item.ModToolMaterial;
-import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpecialPickaxe extends PickaxeItem {
     public SpecialPickaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
-    public static final ToolMaterial material = ModToolMaterial.ADAMANT;
+    public static final ToolMaterial material = ToolMaterials.STONE;
     public static int attackDamage =  3;
     public static float attackSpeed = -2.8f;
 
 
+
+
     @Override
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-            final int x = pos.getX();
+
+
+            int x = pos.getX();
             int y = pos.getY();
             int z = pos.getZ();
-            for(int i = 1; i <= 3; i ++) {
-                for(int j = 1; j <= 3; j++){
-                    world.removeBlock(new BlockPos(x, (y - 2) + i, (z - 2) + j), false);
+            List<BlockPos> Blocks = findBlocks(x, y, z);
+
+            for(int i = 0; i < Blocks.size(); i++){
+                BlockPos bp = Blocks.get(i);
+                if(stack.isSuitableFor(world.getBlockState(bp))){
+                    extraBreak(stack, world, bp, miner, world.getBlockState(bp));
+                    world.breakBlock(bp, true);
+
                 }
+
 
 
             }
@@ -38,5 +55,31 @@ public class SpecialPickaxe extends PickaxeItem {
         }
 
         return true;
+    }
+
+    private void extraBreak(ItemStack i, World w, BlockPos p, LivingEntity m, BlockState s) {
+
+      //  m.sendMessage(Text.literal("not Client? " + !w.isClient + "Hardness for " + w.getBlockState(p).getBlock().getName() +  w + " " + p + s.getHardness(w, p) ));
+        if (!w.isClient && s.getHardness(w, p) != 0.0F) {
+
+            i.damage(1, m, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        }
+    }
+
+    private List<BlockPos> findBlocks( int x, int y, int z) {
+        List<BlockPos> blocks = new ArrayList<>(26);
+        for(int newX = x-1; newX <= x + 1; newX++){
+            for(int newY = y-1; newY <= y + 1; newY++){
+                for(int newZ = z-1; newZ <= z+1; newZ ++){
+                    if(newX == x && newY == y && newZ == z){
+                        continue;
+                    }
+                   // IsaacsMod.LOGGER.info("Adding Block Position x: " + x + " y: " + y + " z: " + z );
+                    blocks.add(new BlockPos(newX, newY, newZ));
+                }
+            }
+
+        }
+        return blocks;
     }
 }
